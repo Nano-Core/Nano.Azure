f6d74c1d89fb855f3e4ce46fda234d08212278be
vivet <vivet@vivetonline.com>
2026-06-19 17:11:50 +0200

    Updated

--- Nano.Azure.ContainerRegistry/README.md
+++ Nano.Azure.ContainerRegistry/README.md
@@ -8,6 +8,7 @@
 * **[Summary](#summary)**  
 * **[Registration](#registration)**  
   * **[Attach Registry to Kubernetes](#attach-registry-to-kubernetes)**  
+  * **[Network Rules](#network-rules)**  
 * **[Dependencies](#dependencies)**  
 
 ## Summary
@@ -42,6 +43,26 @@ Because of this integration, there is no need to define `imagePullSecrets` in de
 imagePullSecrets:
   - name: pull-secret
 ```
+
+## Network Rules
+The containe registry has public network access disabled by default.
+
+A Private Endpoint is created within the Kubernetes virtual network, enabling applications running in the cluster to securely access the Storage Account file shares over a private 
+connection.
+
+To list the available `--group-id` values for use with the `az network private-endpoint create` command, run the following command.  
+
+```powershell
+az network private-link-resource list --id $env:ACR_ID;
+```
+
+> ⚠️ The private endpoint must be deployed in the same Azure region as the virtual network (VNet) it is associated with.
+
+Optionally, IP address whitelisting can be configured to allow access to the container registry. By default, access is fully restricted, and no external connections are permitted. 
+The Nano system does not depend on IP whitelisting for connectivity, and using it is generally discouraged as it can negatively impact the overall cloud security score. If IP 
+whitelisting is required, it can be configured using the following command.  
+
+> ⚠️ Whitelisting IP addresses can reduce the overall security posture and negatively impact the security score.  
 
 ## Dependencies
 Container Registry has the following dependencies that must be deployed or otherwise satisfied prior to setup.  
--- Nano.Azure.ContainerRegistry/deploy.ps1
+++ Nano.Azure.ContainerRegistry/deploy.ps1
@@ -1,8 +1,9 @@
 $env:ENVIRONMENT = "";
 $env:AZURE_LOCATION = "North Europe";
-$env:AZURE_RESOURCE_GROUP_DELIVERY = "Nano-Delivery";
-$env:AZURE_RESOURCE_GROUP_KUBERNETES = "Nano-Kubernetes";
+$env:AZURE_RESOURCE_GROUP = "Nano-Delivery";
 $env:AZURE_RESOURCE_GROUP_LOGS = "Nano-Logs";
+$env:AZURE_RESOURCE_GROUP_KUBERNETES = "Nano-Kubernetes";
+$env:AZURE_RESOURCE_GROUP_KUBERNETES_ASSETS = "Nano-Kubernetes-Assets";
 $env:APP_NAME = "nano" + $env:ENVIRONMENT.ToLower() + "containerregistry";
 
 # Register Providers
@@ -12,18 +13,55 @@ az provider register --namespace Microsoft.ContainerRegistry;
 $env:LOG_ANALYTICS_WORKSPACE_ID = az monitor log-analytics workspace list -g $env:AZURE_RESOURCE_GROUP_LOGS --query [0].id -o tsv;
 
 az acr create `
-  -g $env:AZURE_RESOURCE_GROUP_DELIVERY `
+  -g $env:AZURE_RESOURCE_GROUP `
   -n $env:APP_NAME `
   -l $env:AZURE_LOCATION `
-  --sku Standard `
+  --sku Preimium `
   --workspace $env:LOG_ANALYTICS_WORKSPACE_ID `
   --admin-enabled false;
 
 # Attach ACR to AKS
-$env:CONTAINER_REGISTRY_ID = az acr show -g $env:AZURE_RESOURCE_GROUP_DELIVERY -n $env:APP_NAME --query id -o tsv;
+$env:CONTAINER_REGISTRY_ID = az acr show -g $env:AZURE_RESOURCE_GROUP -n $env:APP_NAME --query id -o tsv;
 $env:KUBERNETES_NAME = az aks list -g $env:AZURE_RESOURCE_GROUP_KUBERNETES --query [0].name -o tsv;
 
 az aks update `
   -n $env:KUBERNETES_NAME `
   -g $env:AZURE_RESOURCE_GROUP_KUBERNETES `
   --attach-acr $env:CONTAINER_REGISTRY_ID;
+
+# Network Rules.
+$env:PRIVATE_LINK = "privatelink.azurecr.io";
+$env:PRIVATE_ENDPOINT_NAME = $env:APP_NAME + "-private-endpoint";
+$env:ACR_ID = az acr show -g $env:AZURE_RESOURCE_GROUP -n $env:APP_NAME --query id -o tsv;
+$env:VNET_ID = az network vnet list -g $env:AZURE_RESOURCE_GROUP_KUBERNETES_ASSETS --query [0].id -o tsv;
+$env:VNET_NAME = az network vnet list -g $env:AZURE_RESOURCE_GROUP_KUBERNETES_ASSETS --query [0].name -o tsv;
+$env:VNET_LOCATION = az network vnet list -g $env:AZURE_RESOURCE_GROUP_KUBERNETES_ASSETS --query [0].location -o tsv;
+$env:SUBNET_ID = az network vnet subnet list -g $env:AZURE_RESOURCE_GROUP_KUBERNETES_ASSETS --vnet-name $env:VNET_NAME --query "[?name =='aks-subnet'].[id]" -o tsv;
+
+az network private-dns zone create `
+    -g $env:AZURE_RESOURCE_GROUP `
+    -n $env:PRIVATE_LINK;
+
+az network private-dns link vnet create `
+    -g $env:AZURE_RESOURCE_GROUP `
+    -n $env:PRIVATE_ENDPOINT_NAME-dns-link `
+    -z $env:PRIVATE_LINK `
+    -v $env:VNET_ID `
+    -e false;
+
+az network private-endpoint create `
+    -l $env:VNET_LOCATION `
+    --name $env:PRIVATE_ENDPOINT_NAME `
+    --connection-name $env:PRIVATE_ENDPOINT_NAME-connection `
+    --nic-name $env:PRIVATE_ENDPOINT_NAME-nic `
+    --resource-group $env:AZURE_RESOURCE_GROUP `
+    --group-id registry `
+    --subnet $env:SUBNET_ID `
+    --private-connection-resource-id $env:ACR_ID;
+
+az network private-endpoint dns-zone-group create `
+    -g $env:AZURE_RESOURCE_GROUP `
+    -n $env:PRIVATE_ENDPOINT_NAME-dns-zone-group `
+    --endpoint-name $env:PRIVATE_ENDPOINT_NAME `
+    --private-dns-zone $env:PRIVATE_LINK `
+    --zone-name registry;
